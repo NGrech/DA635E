01ced0a6afd3252524efd09d0aaba0ca91bdc345
Rafi Khaliqi <rafikhaliqi71@gmail.com>
2022-02-08 14:58:23 +0100

    Add Punctuation and whitespace test cases

--- tdd_lab/rovarspraket/test.py
+++ tdd_lab/rovarspraket/test.py
@@ -1,4 +1,5 @@
 #from typing_extensions import Self
+from sre_parse import WHITESPACE
 import unittest
 from parameterized import parameterized
 from unittest import result
@@ -21,6 +22,8 @@ class TestRovar(unittest.TestCase):
     VOWELS = ['a', 'e', 'i', 'o', 'u', 'ö', 'å', 'ä']
     VOWELS_CAP = ['A', 'E', 'I', 'O', 'U', 'Ö', 'Å', 'Ä']
     NUMBERS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    SYMBOLS = list(string.punctuation)
+    WHITESPACE = list(string.whitespace)
 
     def test_null_input_encode(self):
         # Test null input case
@@ -112,9 +115,37 @@ class TestRovar(unittest.TestCase):
         result = rov.derove(data)
         self.assertEqual(result, expected)
 
-# TODO: Symbols test
+    @parameterized.expand(SYMBOLS)
+    def test_symbols_encode(self,data):
+        # symbols encode test
+        expected = copy.copy(data)
+        rov = rovar.Rovar()
+        result = rov.enrove(data)
+        self.assertEqual(result, expected)
 
-# TODO: White space test 
+    @parameterized.expand(SYMBOLS)
+    def test_symbols_decode(self,data):
+        # symbols decode test
+        expected = copy.copy(data)
+        rov = rovar.Rovar()
+        result = rov.derove(data)
+        self.assertEqual(result, expected)
+
+    @parameterized.expand(WHITESPACE)
+    def test_whitespace_encode(self,data):
+        # whitespace encode test
+        expected = copy.copy(data)
+        rov = rovar.Rovar()
+        result = rov.enrove(data)
+        self.assertEqual(result, expected)
+
+    @parameterized.expand(WHITESPACE)
+    def test_whitespace_decode(self,data):
+        # whitespace decode test
+        expected = copy.copy(data)
+        rov = rovar.Rovar()
+        result = rov.derove(data)
+        self.assertEqual(result, expected)
 
 # TODO: Non empty string test 
 
