572430c18a5164d3b957487e8f162151e3d4221a
Nigel Grech <nigelgrech.ng@gmail.com>
2022-02-09 10:33:50 +0100

    Add empty implementation for clock

--- tdd_lab/tdd_clock/clock/clock.py
+++ tdd_lab/tdd_clock/clock/clock.py
@@ -1,1 +1,34 @@
 # Clock module
+
+import string
+import struct
+from tkinter import NO
+from tkinter.messagebox import RETRY
+from xmlrpc.client import boolean
+
+
+class Time():
+    pass
+
+class Date():
+    pass
+
+class Clock():
+    _the_time:Time
+    _the_date:Date
+    _state:boolean
+
+    def __init__(self) -> None:
+        self._the_time = Time()
+        self._the_date = Date()
+        self._state = 0
+
+    def change_mode(self) -> str:
+        return None
+    
+    def ready(self) ->str:
+        return None
+    
+    def set(p1:int, p2:int, p3:int)-> string:
+        return None
+        
